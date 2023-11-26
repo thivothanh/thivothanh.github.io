b8e580c451f32faf7db823637491a4a963679731
Long Nguyen <long.nduy@karrostech.com>
2023-11-26 13:58:01 +0700

    Implement view counter

--- js/scripts.js
+++ js/scripts.js
@@ -95,5 +95,74 @@
             $('#more-projects').fadeIn(300);
         });
     });
+    const firebaseConfig = {
+        databaseURL: "https://hit-counter-thi-vo-default-rtdb.asia-southeast1.firebasedatabase.app/",
+        apiKey: "AIzaSyA0DnVxVQNmHr1kry0Ip60CyTlU9QEJMsQ",
+        authDomain: "hit-counter-thi-vo.firebaseapp.com",
+        projectId: "hit-counter-thi-vo",
+        storageBucket: "hit-counter-thi-vo.appspot.com",
+        messagingSenderId: "963559412303",
+        appId: "1:963559412303:web:de4190bcaa1290545e103a"
+    };
+    firebase.initializeApp(firebaseConfig);
 
-})(jQuery);
+    const hitCounter = document.getElementById("hit-counter");
+    hitCounter.style.display = "none";
+    const lastViewedOn = document.getElementById("last-viewed-on");
+    lastViewedOn.style.display = "none";
+    const db = firebase.database().ref("totalHit");
+    db.on("value", (snapshot) => {
+        hitCounter.textContent = snapshot.val()?.totalHits;
+        const lastViewedOnFromServer = snapshot.val()?.lastViewedOn;
+        if (lastViewedOnFromServer) {
+            const convertedDate = new Date(lastViewedOnFromServer).toLocaleString("vn-VN", {timeZone: "Asia/Ho_Chi_Minh", hour12: false});
+            lastViewedOn.textContent = convertedDate;
+        }
+    });
+    const userCookieName = "returningVisitor";
+    checkUserCookie(userCookieName);
+
+    function checkUserCookie(userCookieName) {
+        const regEx = new RegExp(userCookieName, "g");
+        const cookieExists = document.cookie.match(regEx);
+        if (cookieExists != null) {
+            hitCounter.style.display = "inline";
+            lastViewedOn.style.display = "inline";
+        } else {
+            createUserCookie(userCookieName);
+            db.transaction(
+                (totalHit) => {
+                    const totalHits = totalHit?.totalHits || 0;
+                    const lastViewedOn = new Date().toUTCString();
+                    const newTotalHits = totalHits + 1;
+                    return {
+                        totalHits: newTotalHits,
+                        lastViewedOn
+                    }
+                },
+                (error) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        hitCounter.style.display = "inline";
+                        lastViewedOn.style.display = "inline";
+                    }
+                }
+            );
+        }
+    }
+
+    function createUserCookie(userCookieName) {
+        const userCookieValue = "Yes";
+        const userCookieDays = 7;
+        let expiryDate = new Date();
+        expiryDate.setDate(expiryDate.getDate() + userCookieDays);
+        document.cookie =
+            userCookieName +
+            "=" +
+            userCookieValue +
+            "; expires=" +
+            expiryDate.toGMTString() +
+            "path=/";
+    }
+})(jQuery, firebase);
